dbecca3c6f21ee3cee8a61c1db5dde3252a1d331
Ariko <ariko@vivaldi.net>
2021-03-03 19:46:11 +0100

    improvements in music functions

--- src/commands/music.ts
+++ src/commands/music.ts
@@ -1,7 +1,7 @@
 import { LogService, MatrixClient, MentionPill, MessageEvent, MessageEventContent } from "matrix-bot-sdk";
 import * as htmlEscape from "escape-html";
 import config from "../config";
-const fs = require('fs')
+const fs = require('fs');
 const appFunction = require("../global");
 const lilynode = require("lilynode");
 const musicPath = config.dataPath + "/music";
@@ -12,6 +12,23 @@ export async function runMusicCommand(roomId: string, event: MessageEvent<Messag
     let musicNumber = args[1];
     if (!musicNumber) return appFunction.sendSimpleMessage(client, roomId, "Bwouf, il me faut un numéro de chanson à aller chercher !");
     
+    let musicOption = "";
+    let mime = "";
+    let extension = "";
+    let msgType = "";
+    
+    if ( args[2] && args[2] === "midi" ) {
+        musicOption = "midi";
+        extension = ".mid";
+        mime = "audio/midi";
+        msgType = "m.audio";
+    } else {
+        musicOption = "png";
+        extension = ".png";
+        mime = "image/png"; //image/svg+xml for svg
+        msgType = "m.image";
+    }
+    
     let musicFilePath = musicPath + "/" + musicNumber + ".ly";
     try {
         fs.access(musicPath, fs.F_OK, (err) => {
@@ -19,18 +36,20 @@ export async function runMusicCommand(roomId: string, event: MessageEvent<Messag
                 LogService.error(err);
                 return appFunction.sendSimpleMessage(client, roomId, "Bwouf, je ne connais pas cette chanson !");
             }
-            // if file exists :
-            let mime = "image/svg+xml";
-            lilynode.renderFile(musicFilePath, { format: "svg"}, async function(error, output){
+            
+            
+            // if file exists :            
+            lilynode.renderFile(musicFilePath, { format: musicOption}, async function(error, output){
                 if (error) {
                     LogService.error(error);
                     return appFunction.sendSimpleMessage(client, roomId, "Bwouf, il y a eu un problème !");
                 } else {
-                    let mxc = await client.uploadContent(output, mime, musicNumber + ".svg");
+                    let mxc = await client.uploadContent(output, mime, musicNumber + extension );
+                    fs.writeFileSync(musicPath + "/" + musicNumber + extension, output);
                     LogService.error(mxc);
                     return client.sendMessage(roomId, {
-                        body: musicNumber+ ".svg",
-                        msgtype: "m.image",
+                        body: musicNumber + extension,
+                        msgtype: msgType,
                         url: mxc,
                         info: {
                             mimetype: mime,
